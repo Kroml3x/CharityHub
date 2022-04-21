1a5ffca347b28c4d38f2814ea0d8801c957045bb
Michelepal <Mikaelquasimodus@gmail.com>
2022-04-21 18:54:04 +0200

    inserimento classe associations e metodo get

---
+++ src/app/class/association.ts
@@ -0,0 +1,14 @@
+export class Association {
+
+    id: string;
+    value: string;
+    reg: string;
+
+    constructor(id: string, value: string, reg: string) {
+
+        this.id = id;
+        this.value = value;
+        this.reg = reg;
+
+    }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Association} from '../class/association'
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  associations: Association[]=[];
+
+  constructor(private httpClient: HttpClient) { 
+
+    const url = require('../../server/db.json');
+
+    this.httpClient.get<Association[]>(url).subscribe(data => this.associations=data);
+  }
 
   ngOnInit(): void {
   }
